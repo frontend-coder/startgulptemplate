1096411531de96e3cf2e8a12a9b55a9ab41801be
Oleg <frontendercode@gmail.com>
2019-07-23 12:04:00 +0300

    v.0.3
    Настроена задача по размещению файлов шаблона на хостинге

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,6 @@ const plumber           = require('gulp-plumber');
 const filesize          = require('gulp-filesize');
 const notify                = require('gulp-notify');
 const gulpUtil          = require('gulp-util');
-//const growl          = require('gulp-notify-growl');
 
 // npm i --save-dev browser-sync
 const browserSync           = require('browser-sync').create();
@@ -21,27 +20,28 @@ const browserSync           = require('browser-sync').create();
 // npm i --save-dev del
 const del               = require('del');
 
-
-
+// npm install --save-dev gulp-ftp vinyl-ftp
+const ftp               = require('gulp-ftp');
+const vinyFTP           = require( 'vinyl-ftp' );
 
 
 gulp.task('styles', () => {
-  var sassFiles = [
-  'app/scss/libs.scss',
-  'app/scss/main.scss'
-  ];
-  return gulp.src(sassFiles)
-  .pipe(plumber({
-   errorHandler: notify.onError({
-    message: function(error) {
-      return error.message;
-    }})
- }))
-  .pipe(sourcemaps.init())
-  .pipe(sass({ outputStyle: 'expanded' }))
-.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade:true}))
-.pipe(concat('libs.css'))
-.pipe(rename('libs.min.css'))
+	var sassFiles = [
+	'app/scss/libs.scss',
+	'app/scss/main.scss'
+	];
+	return gulp.src(sassFiles)
+	.pipe(plumber({
+		errorHandler: notify.onError({
+			message: function(error) {
+				return error.message;
+			}})
+	}))
+	.pipe(sourcemaps.init())
+	.pipe(sass({ outputStyle: 'expanded' }))
+	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade:true}))
+	.pipe(concat('libs.css'))
+	.pipe(rename('libs.min.css'))
 //.pipe(cleancss( {level: { 2: { specialComments: 0 } } })) // Opt., comment out when debugging
 .pipe(filesize()).on('error', gulpUtil.log)
 .pipe(sourcemaps.write(''))
@@ -50,8 +50,8 @@ gulp.task('styles', () => {
 });
 
 gulp.task('scripts', done => {
-  var jsFiles = [
-  'app/libs/plagins/jquery/jquery.min.js',
+	var jsFiles = [
+	'app/libs/plagins/jquery/jquery.min.js',
 //'app/libs/plagins/nicescroll/jquery.nicescroll.min.js',
 //'app/libs/plagins/jquery.PageScroll2id/jquery.PageScroll2id.min.js',
 'app/libs/plagins/magnific-popup/jquery.magnific-popup.min.js',
@@ -70,78 +70,72 @@ done();
 });
 
 gulp.task('serve', done => {
-  browserSync.init({
-    server: {
-      baseDir: './app'
-    },
-    notify: false,
-    open:true,
+	browserSync.init({
+		server: {
+			baseDir: './app'
+		},
+		notify: false,
+		open:true,
     // online: false, // Work Offline Without Internet Connection
     // tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
-      });
-  browserSync.watch('app', browserSync.reload);
-  done();
+});
+	browserSync.watch('app', browserSync.reload);
+	done();
 });
 
 gulp.task('code', done => {
 	return gulp.src(['app/*.html', 'app/*php']);
-  done();
+	done();
 });
 
 gulp.task('picture', done => {
-  return gulp.src(['app/img/*.{jpg,png,svg,ico}']);
-  done();
+	return gulp.src(['app/img/*.{jpg,png,svg,ico}']);
+	done();
 });
 
 
 gulp.task('watch', done => {
-  gulp.watch("app/scss/**/*.scss", gulp.series('styles'));
-  gulp.watch("app/libs/**/*.js", gulp.series('scripts'));
-  gulp.watch("app/*.html", gulp.series('code'));
-  gulp.watch("app/img/**/*.*", gulp.series('picture'));
-  done();
+	gulp.watch("app/scss/**/*.scss", gulp.series('styles'));
+	gulp.watch("app/libs/**/*.js", gulp.series('scripts'));
+	gulp.watch("app/*.html", gulp.series('code'));
+	gulp.watch("app/img/**/*.*", gulp.series('picture'));
+	done();
 });
 
 gulp.task('default', gulp.parallel(['styles','scripts', 'watch', 'serve']));
 
 function cleaner() {
-  return del('dist/*');
+	return del('dist/*');
 }
 
 function movefile() {
 	return gulp.src('app/*.html')
-       .pipe(gulp.dest('dist'));
-     }
-
-     function movefilother() {
-      return gulp.src('app/*.{php,access}')
-      .pipe(gulp.dest('dist'));
-    }
-
-    function movejs() {
-      return gulp.src('app/js/scripts.min.js')
-  //  .pipe(uglify()) // Mifify js (opt.)
-    .pipe(gulp.dest('dist/js'))
-    .pipe(filesize()).on('error', gulpUtil.log);
-  }
-  function movecss() {
-    return gulp.src('app/css/*')
- //  .pipe(cleancss( {level: { 2: { specialComments: 0 } } })) // Opt., comment out when debugging
-   .pipe(gulp.dest('dist/css'))
-   .pipe(filesize()).on('error', gulpUtil.log);
- }
-
- function moveimages() {
-  return gulp.src('app/img/**/*.{jpg,svg,png,ico}')
-.pipe(gulp.dest('dist/img'))
-.pipe(filesize()).on('error', gulpUtil.log);
+	.pipe(gulp.dest('dist'));
 }
 
-// function compressimg() {
-//  return gulp.src('app/beforecompress/**/*')
-//  .pipe(tingpng('8cVpmwZQXvCdnVDk2FqdbWVk5RfJBS9Z'))
-//  .pipe(gulp.dest('dist/aftercompress'));
-// }
+function movefilother() {
+	return gulp.src('app/*.{php,access}')
+	.pipe(gulp.dest('dist'));
+}
+
+function movejs() {
+	return gulp.src('app/js/scripts.min.js')
+  //  .pipe(uglify()) // Mifify js (opt.)
+  .pipe(gulp.dest('dist/js'))
+  .pipe(filesize()).on('error', gulpUtil.log);
+}
+function movecss() {
+	return gulp.src('app/css/*')
+ //  .pipe(cleancss( {level: { 2: { specialComments: 0 } } })) // Opt., comment out when debugging
+ .pipe(gulp.dest('dist/css'))
+ .pipe(filesize()).on('error', gulpUtil.log);
+}
+
+function moveimages() {
+	return gulp.src('app/img/**/*.{jpg,svg,png,ico}')
+	.pipe(gulp.dest('dist/img'))
+	.pipe(filesize()).on('error', gulpUtil.log);
+}
 
 // gulp.task('compressimg', gulp.series(compressimg));
 gulp.task('cleanbuild', cleaner);
@@ -153,8 +147,29 @@ gulp.task('moveimages', gulp.series(moveimages));
 
 gulp.task('build', gulp.series('cleanbuild', gulp.parallel('movefile', 'movefilother', 'movejs', 'movecss', 'moveimages' )));
 
+// FTP: ftp://vh146.timeweb.ru
+// Логин: cc63120
+// Пароль: j7X4Y36Od5Zm
+// http://cw25156.tmweb.ru/
 
+gulp.task( 'ftp', function () {
+	var conn = vinyFTP.create( {
+		host:     'vh210.timeweb.ru',
+		user:     'cw25156',
+		password: '2qzRb2Wo2zjm',
+		parallel: 10,
+		log:      gulpUtil.log
+	} );
 
+	var globs = [
+	'dist/**'
+	];
 
+    // using base = '.' will transfer everything to /public_html correctly
+    // turn off buffering in gulp.src for best performance
 
+    return gulp.src( globs, { base: './dist/', buffer: false } )
+        .pipe( conn.newerOrDifferentSize( '/public_html' ) )// only upload newer files
+        .pipe( conn.dest( '/public_html' ) );
 
+    } );
