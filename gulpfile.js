e9a47fc7aa9a4ef0760e221c56c269328da3223e
Oleg <frontendercode@gmail.com>
2020-02-07 15:11:17 +0200

    v.0.32
    Исправил ошибки работы Gulp плагинов

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
+"use strict";
 // npm i gulp --save-dev
-const gulp              = require('gulp');
+const gulp             = require('gulp');
 // npm i --save-dev gulp-sass gulp-concat gulp-uglify gulp-clean-css gulp-rename gulp-autoprefixer gulp-sourcemaps gulp-plumber gulp-filesize gulp-notify
 // npm i --save-dev gulp-util
-const sass                  = require('gulp-sass');
-const concat                = require('gulp-concat');
-const uglify                = require('gulp-uglify');
-const cleancss              = require('gulp-clean-css');
-const rename                = require('gulp-rename');
-const autoprefixer          = require('gulp-autoprefixer');
-const sourcemaps        = require('gulp-sourcemaps');
-const plumber           = require('gulp-plumber');
-const filesize          = require('gulp-filesize');
-const notify                = require('gulp-notify');
-const gulpUtil          = require('gulp-util');
+const sass             = require('gulp-sass');
+const cssbeautify      = require('gulp-cssbeautify');
+const stripCssComments = require('gulp-strip-css-comments');
+const strip            = require('gulp-strip-comments');
+const concat           = require('gulp-concat');
+const uglify           = require('gulp-uglify');
+const rigger = require('gulp-rigger');
+const cleancss         = require('gulp-clean-css');
+const rename           = require('gulp-rename');
+const autoprefixer     = require('gulp-autoprefixer');
+const sourcemaps       = require('gulp-sourcemaps');
+const plumber          = require('gulp-plumber');
+const filesize         = require('gulp-filesize');
+const notify           = require('gulp-notify');
+const gulpUtil         = require('gulp-util');
 
 // npm i --save-dev browser-sync
 const browserSync           = require('browser-sync').create();
@@ -32,24 +37,27 @@ gulp.task('styles', () => {
 	];
 	return gulp.src(sassFiles)
 	.pipe(plumber({
-		errorHandler: notify.onError({
-			message: function(error) {
-				return error.message;
-			}})
+		errorHandler: notify.onError(function(err){
+			return {
+				title: 'Styles',
+				message:err.message
+			}
+			})
 	}))
 	.pipe(sourcemaps.init())
 	.pipe(sass({ outputStyle: 'expanded' }))
-	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade:true}))
+	.pipe(autoprefixer(['last 6 versions', '> 1%', 'ie 8', 'ie 7'], {cascade:true}))
+	.pipe(stripCssComments())
+	.pipe(cssbeautify({indent: '  ', openbrace: 'separate-line', autosemicolon: true}))
 	.pipe(concat('libs.css'))
 	.pipe(rename('libs.min.css'))
 //.pipe(cleancss( {level: { 2: { specialComments: 0 } } })) // Opt., comment out when debugging
-.pipe(filesize()).on('error', gulpUtil.log)
 .pipe(sourcemaps.write(''))
-.pipe(notify("Create file: <%= file.relative %>!"))
+//.pipe(notify("Create file: <%= file.relative %>!"))
 .pipe(gulp.dest('app/css'));
 });
 
-gulp.task('scripts', done => {
+gulp.task('scripts', (done) => {
 	var jsFiles = [
 	'app/libs/plagins/jquery341.js',
 'app/libs/plagins/page-scroll-to-id-master/js/minified/jquery.malihu.PageScroll2id.min.js',
@@ -59,12 +67,19 @@ gulp.task('scripts', done => {
 // Always at the end
 ];
 return gulp.src(jsFiles)
-.pipe(concat('scripts.min.js'))
+.pipe(strip())
+	.pipe(plumber({
+		errorHandler: notify.onError({
+			message: function(error) {
+				return error.message;
+			}})
+	}))
+.pipe(rigger())
+.pipe(concat('scripts.min.js').on('error', done))
 //	.pipe(uglify()) // Mifify js (opt.)
-.pipe(notify("Create file: <%= file.relative %>!"))
+.pipe(notify("Create file: <%= file.relative %>!").on('error', gulpUtil.log) )
 .pipe(gulp.dest('app/js'))
 .pipe(filesize()).on('error', gulpUtil.log);
-done();
 });
 
 gulp.task('serve', done => {
@@ -108,6 +123,7 @@ function cleaner() {
 
 function movefile() {
 	return gulp.src('app/*.html')
+	.pipe(strip())
 	.pipe(gulp.dest('dist'));
 }
 
