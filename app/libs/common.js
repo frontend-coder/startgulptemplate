d3eae91457ede97f413cfa6f88cf2cf3cf6e507e
Oleg <frontendercode@gmail.com>
2020-06-30 20:30:25 +0300

    v.0.40
    Переписал все функции и задачи, ориентируясь на современные требования  Gulp v.4.0

--- app/libs/common.js
+++ app/libs/common.js
@@ -1,3 +1,6 @@
 $(document).ready(() => {
-
+  const io = 2;
+  console.log(3000);
+  console.log(io);
+  let klik;
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,192 +1,155 @@
 "use strict";
 // npm i gulp --save-dev
-const gulp             = require('gulp');
-// npm i --save-dev gulp-sass gulp-concat gulp-uglify gulp-clean-css gulp-rename gulp-autoprefixer gulp-sourcemaps gulp-plumber gulp-filesize gulp-notify
-// npm i --save-dev gulp-util
-const sass             = require('gulp-sass');
-const cssbeautify      = require('gulp-cssbeautify');
-const stripCssComments = require('gulp-strip-css-comments');
-const strip            = require('gulp-strip-comments');
-const concat           = require('gulp-concat');
-const uglify           = require('gulp-uglify');
-const rigger = require('gulp-rigger');
-const cleancss         = require('gulp-clean-css');
-const rename           = require('gulp-rename');
-const autoprefixer     = require('gulp-autoprefixer');
-const sourcemaps       = require('gulp-sourcemaps');
-const plumber          = require('gulp-plumber');
-const filesize         = require('gulp-filesize');
-const notify           = require('gulp-notify');
-const gulpUtil         = require('gulp-util');
+const { src, dest, parallel, series, watch  } = require('gulp');
 
-// npm i --save-dev browser-sync
-const browserSync           = require('browser-sync').create();
+const concat                                  = require('gulp-concat');
+let uglify                                    = require('gulp-uglify-es').default;
+const rigger                                  = require('gulp-rigger');
 
-// npm i --save-dev del
-const del               = require('del');
+const sass                                    = require('gulp-sass');
+const autoprefixer                            = require('gulp-autoprefixer');
+const sourcemaps                              = require('gulp-sourcemaps');
+const rename                                  = require('gulp-rename');
 
-// npm install --save-dev gulp-ftp vinyl-ftp
-const ftp               = require('gulp-ftp');
-const vinyFTP           = require( 'vinyl-ftp' );
 
+const stripCssComments                        = require('gulp-strip-css-comments');
+const cleancss                                = require('gulp-clean-css');
+const strip                                   = require('gulp-strip-comments');
+const imagemin                                = require('gulp-imagemin');
+const newer                                   = require('gulp-newer');
+const del                                     = require('del');
+const plumber                                 = require('gulp-plumber');
+const notify                                  = require('gulp-notify');
+
+const ftp                                     = require('gulp-ftp');
+const vinyFTP                                 = require('vinyl-ftp');
+const gulpUtil                                = require('gulp-util');
+
+const browserSync                             = require('browser-sync').create();
 
 function styles() {
-	let sassFiles = [
-	'app/scss/libs.scss',
-	'app/scss/main.scss'
-	];
-	return gulp.src(sassFiles)
-	.pipe(plumber({
+	return src([
+		'app/scss/libs.scss',
+		'app/scss/main.scss'
+	])
+		.pipe(plumber({
 		errorHandler: notify.onError(function(err){
-			return {
-				title: 'Styles',
-				message:err.message
-			}
-			})
-	}))
-	.pipe(sourcemaps.init())
-	.pipe(sass({ outputStyle: 'expanded' }))
-	.pipe(autoprefixer(['last 6 versions', '> 1%', 'ie 8', 'ie 7'], {cascade:true}))
-	.pipe(stripCssComments())
-	.pipe(cssbeautify({indent: '  ', openbrace: 'separate-line', autosemicolon: true}))
-	.pipe(concat('libs.css'))
-	.pipe(rename('libs.min.css'))
-//.pipe(cleancss( {level: { 2: { specialComments: 0 } } })) // Opt., comment out when debugging
-.pipe(sourcemaps.write(''))
-//.pipe(notify("Create file: <%= file.relative %>!"))
-.pipe(gulp.dest('app/css'));
-};
+ 			return {
+ 				title: 'Styles',
+ 				message:err.message
+ 			}
+ 			})
+ 	}))
+		.pipe(sourcemaps.init())
+		  .pipe(sass().on('error', sass.logError))
+		.pipe(concat('libs.css'))
+		.pipe(rename('libs.min.css'))
+		.pipe(autoprefixer({
+			overrideBrowserlist: ['last 10 versions'],
+			grid: true
+		}))
+		.pipe(stripCssComments())
+		.pipe(cleancss(({
+			level: { 2: { specialComments: 0 } },
+//			format: 'beautify'
+			format: 'keep-breaks'
+		}))) // Opt., comment out when debugging
+		.pipe(sourcemaps.write(''))
+		.pipe(dest('app/css/'))
+}
+
+function browsersync() {
+	browserSync.init({
+		server: { baseDir: 'app/' },
+		notify: false,
+		online: false
+	})
+}
 
 function scripts() {
-	var jsFiles = [
-	'app/libs/plagins/jquery.min.js',
-// 'app/libs/plagins/page-scroll-to-id-master/js/minified/jquery.malihu.PageScroll2id.min.js',
-// 'app/libs/plagins/magnific-popup/jquery.magnific-popup.min.js',
-// 'app/libs/plagins/slick/slick.min.js',
-'app/libs/common.js'
-// Always at the end
-];
-return gulp.src(jsFiles)
-.pipe(strip())
-	.pipe(plumber({
-		errorHandler: notify.onError({
-			message: function(error) {
-				return error.message;
-			}})
-	}))
-.pipe(rigger())
-.pipe(concat('scripts.min.js'))
-//	.pipe(uglify()) // Mifify js (opt.)
-.pipe(notify("Create file: <%= file.relative %>!").on('error', gulpUtil.log) )
-.pipe(gulp.dest('app/js'))
-.pipe(filesize()).on('error', gulpUtil.log);
-};
-
-function serve() {
-	browserSync.init({
-		server: {
-			baseDir: './app'
-		},
-		notify: false,
-		open:true,
-    // online: false, // Work Offline Without Internet Connection
-    // tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
-});
-	browserSync.watch('app', browserSync.reload);
-
-};
-
-function code() {
-	return gulp.src(['app/*.html', 'app/*php']);
-};
-
-function picture() {
-	return gulp.src(['app/img/*.{jpg,png,svg,ico}']);
-};
-
-function watch() {
-	gulp.watch("app/scss/**/*.scss", gulp.series('styles'));
-	gulp.watch("app/libs/**/*.js", gulp.series('scripts'));
-	gulp.watch("app/*.html", gulp.series('code'));
-	gulp.watch("app/img/**/*.*", gulp.series('picture'));
-};
-
-function cleaner() {
-	return del('dist/*');
+	return src([
+		'app/libs/plagins/jquery.min.js',
+		'app/libs/plagins/page-scroll-to-id-master/js/minified/jquery.malihu.PageScroll2id.min.js',
+		'app/libs/plagins/magnific-popup/jquery.magnific-popup.min.js',
+		'app/libs/plagins/slick/slick.min.js',
+		'app/libs/common.js'
+	])
+		.pipe(strip())
+		.pipe(rigger())
+		.pipe(concat('scripts.min.js'))
+		.pipe(uglify())
+		.pipe(dest('app/js/'))
+		.pipe(browserSync.stream())
 }
 
-function movefile() {
-	return gulp.src('app/*.html')
-	.pipe(strip())
-	.pipe(gulp.dest('dist'));
+function images() {
+	return src('app/img/**/*')
+		.pipe(newer('dist/img/'))
+		.pipe(imagemin())
+		.pipe(dest('dist/img/'))
 }
 
-function movefilother() {
-	return gulp.src('app/*.{php,access}')
-	.pipe(gulp.dest('dist'));
+function cleanimg() {
+	return del('dist/img/')
 }
 
-function movejs() {
-	return gulp.src('app/js/scripts.min.js')
-  //  .pipe(uglify()) // Mifify js (opt.)
-  .pipe(gulp.dest('dist/js'))
-  .pipe(filesize()).on('error', gulpUtil.log);
-}
-function movecss() {
-	return gulp.src('app/css/*')
- //  .pipe(cleancss( {level: { 2: { specialComments: 0 } } })) // Opt., comment out when debugging
- .pipe(gulp.dest('dist/css'))
- .pipe(filesize()).on('error', gulpUtil.log);
+function cleandest() {
+  return del('dest/**/*', { force : true })
 }
 
-function moveimages() {
-	return gulp.src('app/img/**/*.{jpg,svg,png,ico}')
-	.pipe(gulp.dest('dist/img'))
-	.pipe(filesize()).on('error', gulpUtil.log);
+function startwatch() {
+	watch('app/scss/*.scss', styles);
+	watch('app/img/**/*.*').on('change', browserSync.reload);
+	watch('app/**/*.html').on('change', browserSync.reload);
+	watch('app/**/*.php').on('change', browserSync.reload);
+	watch('app/css/*.css').on('change', browserSync.reload);
+	watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+ }
+
+function buildcopy() {
+	return src(['app/css/**/*.min.css',
+		'app/js/**/*.min.js',
+		'app/**/*.html',
+		'app/**/*.php',
+		'app/**/ht.access'], {base:'app'})
+.pipe(dest('dest'))
 }
 
-gulp.task('styles', styles);
-gulp.task('scripts', scripts);
-gulp.task('serve', serve);
-gulp.task('code', code);
-gulp.task('picture', picture);
-gulp.task('watch', watch);
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.images = images;
+exports.cleanimg = cleanimg;
+exports.cleandest = cleandest;
 
-gulp.task('default', gulp.parallel(['styles','scripts', 'watch', 'serve']));
+exports.build = series(cleandest, styles, scripts, images, buildcopy);
 
-// gulp.task('compressimg', gulp.series(compressimg));
-gulp.task('cleanbuild', cleaner);
-gulp.task('movefile', movefile);
-gulp.task('movefilother', movefilother);
-gulp.task('movejs', movejs);
-gulp.task('movecss', movecss);
-gulp.task('moveimages', gulp.series(moveimages));
-
-gulp.task('build', gulp.series('cleanbuild', gulp.parallel('movefile', 'movefilother', 'movejs', 'movecss', 'moveimages' )));
+exports.default = parallel(styles, scripts, browsersync, startwatch)
 
 // FTP: ftp://vh146.timeweb.ru
 // Логин: cc63120
 // Пароль: j7X4Y36Od5Zm
 // http://cw25156.tmweb.ru/
 
-gulp.task( 'ftp', function () {
-	var conn = vinyFTP.create( {
-		host:     'vh210.timeweb.ru',
-		user:     'cw25156',
-		password: '2qzRb2Wo2zjm',
-		parallel: 10,
-		log:      gulpUtil.log
+function ftp() {
+let conn = vinyFTP.create( {
+	host:     'vh210.timeweb.ru',
+	user:     'cw25156',
+	password: '2qzRb2Wo2zjm',
+	parallel: 10,
+	log:      gulpUtil.log
 	} );
 
-	var globs = [
+	let globs = [
 	'dist/**'
 	];
 
-    // using base = '.' will transfer everything to /public_html correctly
-    // turn off buffering in gulp.src for best performance
+ // using base = '.' will transfer everything to /public_html correctly
+ // turn off buffering in gulp.src for best performance
 
     return gulp.src( globs, { base: './dist/', buffer: false } )
-        .pipe( conn.newerOrDifferentSize( '/public_html' ) )// only upload newer files
-        .pipe( conn.dest( '/public_html' ) );
+   .pipe( conn.newerOrDifferentSize( '/public_html' ) )// only upload newer files
+   .pipe( conn.dest( '/public_html' ) );
+  };
 
-    } );
+exports.ftp = ftp;
