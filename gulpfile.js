475fd5c7b7f87538838f503810c71a57cd2f4e13
frontend-coder <frontendercode@gmail.com>
2023-10-23 14:23:31 +0300

    v.0.55
    Перероблені завдання оптимізаціі зображень, шрифтів, підключення спрайтів та компонентів html сторінок

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,29 @@ const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 const browserSync = require('browser-sync').create();
 
+const avif = require('gulp-avif');
+const webp = require('gulp-webp');
+const svgSprite = require('gulp-svg-sprite');
+
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+
+const fonter = require('gulp-fonter');
+const ttf2woff2 = require('gulp-ttf2woff2');
+const include = require('gulp-include');
+
+function pages() {
+  return src('app/pages/*.html')
+    .pipe(
+      include({
+        includePaths: 'app/components',
+      })
+    )
+    .pipe(dest('app'));
+  //    .pipe(browserSync.stream()) ;
+}
+
 function styles() {
   return src(['app/scss/libs.scss', 'app/scss/main.scss'])
     .pipe(
@@ -78,12 +101,83 @@ function scripts() {
     .pipe(browserSync.stream());
 }
 
-function images() {
-  return src('app/img/**/*').pipe(newer('dest/img/')).pipe(imageminPic()).pipe(dest('dest/img/'));
+function picture() {
+  return src(['app/img/src/*.*', '!app/img/src/*.svg'])
+    .pipe(newer('app/img'))
+    .pipe(avif({ quality: 50 }))
+    .pipe(src('app/img/src/*.*'))
+    .pipe(newer('app/img'))
+    .pipe(webp())
+    .pipe(src('app/img/src/*.*'))
+    .pipe(newer('app/img'))
+    .pipe(imageminPic())
+    .pipe(dest('app/img'));
 }
 
-function cleanimg() {
-  return del('dist/img/');
+function sprite() {
+  return (
+    src('app/img/sprite/*.svg')
+      // minify svg
+      .pipe(
+        svgmin({
+          js2svg: {
+            pretty: true,
+          },
+        })
+      )
+      // remove all fill and style declarations in out shapes
+      .pipe(
+        cheerio({
+          run: function ($) {
+            $('[fill]').removeAttr('fill');
+            $('[stroke]').removeAttr('stroke');
+            $('[style]').removeAttr('style');
+          },
+          parserOptions: { xmlMode: true },
+        })
+      )
+      // cheerio plugin create unnecessary string '&gt;', so replace it.
+      .pipe(replace('&gt;', '>'))
+      // build svg sprite
+      .pipe(
+        svgSprite({
+          mode: {
+            symbol: {
+              sprite: '../sprite.svg',
+              render: {
+                scss: {
+                  dest: 'app/scss/_sprite.scss',
+                  template: 'app/scss/templates/_sprite_template.scss',
+                },
+              },
+              example: true,
+            },
+          },
+        })
+      )
+
+      .pipe(dest('app/img/'))
+  );
+}
+
+function fonts() {
+  return src('app/fonts/src/*.*')
+    .pipe(
+      fonter({
+        formats: ['woff', 'ttf'],
+      })
+    )
+    .pipe(src('app/fonts/*.ttf'))
+    .pipe(ttf2woff2())
+    .pipe(dest('app/fonts'));
+}
+
+function images() {
+  return src(['app/img/**/*.*', '!app/img/sprite/**/*.*', '!app/img/src/**/*.*']).pipe(newer('dest/img/')).pipe(dest('dest/img/'));
+}
+
+function cleaning() {
+  return del('dest/img/');
 }
 
 function cleandest() {
@@ -91,8 +185,13 @@ function cleandest() {
 }
 
 function startwatch() {
+  watch('app/fonts/src/*.*', fonts);
+  watch('app/img/*.*', picture);
+  watch('app/img/src/*.*', picture);
+  watch(['app/components/*.*', 'app/pages/*.*'], pages);
+  watch('app/img/favicon/*.*').on('change', browserSync.reload);
+
   watch('app/scss/*.scss', styles);
-  watch('app/img/**/*.*').on('change', browserSync.reload);
   watch('app/**/*.html').on('change', browserSync.reload);
   watch('app/**/*.php').on('change', browserSync.reload);
   watch('app/css/*.css').on('change', browserSync.reload);
@@ -108,9 +207,14 @@ exports.browsersync = browsersync;
 exports.scripts = scripts;
 exports.styles = styles;
 exports.images = images;
-exports.cleanimg = cleanimg;
+exports.cleaning = cleaning;
 exports.cleandest = cleandest;
+exports.picture = picture;
+
+exports.sprite = sprite;
+exports.fonts = fonts;
+exports.pages = pages;
 
 exports.build = series(cleandest, styles, scripts, images, buildcopy);
 
-exports.default = parallel(styles, scripts, browsersync, startwatch);
+exports.default = parallel(styles, scripts, picture, pages, browsersync, startwatch);
